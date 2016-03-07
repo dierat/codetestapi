a6fbb0f626125c804695be3d324a6e96fe539fb7
dierat <diedra.rater@gmail.com>
2016-03-06 21:33:55 -0800

    improves comments for clarity

--- meteor-server/server/app.js
+++ meteor-server/server/app.js
@@ -8,7 +8,8 @@ import esprima from 'esprima';
     shouldHave: [],
     shouldNotHave: [],
   }
-'analyzeCode' should return true if the code passed in matches all the requirements
+
+'analyzeCode' will return true if the code passed in matches all the requirements
 and false if it does not
 
 */
@@ -16,11 +17,11 @@ const analyzeCode = function(code, functionality){
   // parse the student-written code to more easily analyze what it contains
   const parsed = esprima.parse(code);
 
-  // add each type from input code to an object literal (for faster checking)
+  // add each type from input code as keys to an object literal (for faster checking)
   const codeTypes = {};
   parsed.body.forEach((codeNode)=> codeTypes[ codeNode.type ] = true);
 
-  // check if each shouldHave is in input code, otherwise return false
+  // check if each shouldHave is in input code; if not, return false
   if (functionality.shouldHave){
     for (let i = 0; i < functionality.shouldHave.length; i++){
       const type = functionality.shouldHave[i];
@@ -28,7 +29,7 @@ const analyzeCode = function(code, functionality){
     }
   }
 
-  // check if each shouldNotHave isn't in input code, otherwise return false
+  // check if each shouldNotHave isn't in input code; if not, return false
   if (functionality.shouldNotHave){
     for (let j = 0; j < functionality.shouldNotHave.length; j++){
       const type = functionality.shouldNotHave[j];
@@ -36,6 +37,6 @@ const analyzeCode = function(code, functionality){
     }
   }
 
-  // if the function has not yet returned false, return true
+  // if the code input passed the checks, return true
   return true;
 };
