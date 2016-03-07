b2190c5cb76dcff70f05bcae3f546a653d2d9327
dierat <diedra.rater@gmail.com>
2016-03-06 21:52:19 -0800

    defines some code strings to test against

--- meteor-server/server/test.js
+++ meteor-server/server/test.js
@@ -1,2 +1,29 @@
 import speck from 'speckjs';
 import {analyzeCode} from "./app";
+
+
+const varDeclaration = `var x = false;`;
+
+const ifStatement = `
+  if (true){
+    console.log("it's true!");
+  }
+`;
+
+const whileLoop = `
+  while (true){
+    console.log("it's true!");
+  }
+`;
+
+const forLoop = `
+  for (let i = 0; i < 5; i++){
+    console.log(i);
+  }
+`;
+
+const forLoopWithIf = `
+  for (let i = 0; i < 5; i++){
+    if (i % 2 === 0) console.log(i);
+  }
+`;
