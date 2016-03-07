8b6d05eb9b2831ad2f199952620222f3f56fc282
dierat <diedra.rater@gmail.com>
2016-03-06 19:44:24 -0800

    update pseudocode to explanatory comments

--- meteor-server/server/app.js
+++ meteor-server/server/app.js
@@ -16,22 +16,17 @@ const analyzeCode = function(code, functionality){
   // parse the student-written code to more easily analyze what it contains
   const parsed = esprima.parse(code);
 
-  // define an object literal to hold the types of code in the 'code' input
+  // add each type from input code to an object literal (for faster checking)
   const codeTypes = {};
-
-  // for each object literal's 'type' value in parsed.body,
-  // add it to the code types object literal
   parsed.body.forEach((codeNode)=> codeTypes[ codeNode.type ] = true);
 
-  // for each of the shouldHave's,
+  // check if each shouldHave is in input code, otherwise return false
   functionality.shouldHave.forEach((type)=>{
-    // if it isn't in the code types object literal, return false
     if ( !(type in codeTypes) ) return false;
   });
 
-  // for each of the shouldNotHave's,
+  // check if each shouldNotHave isn't in input code, otherwise return false
   functionality.shouldNotHave.forEach((type)=>{
-    // if it's in the code types object literal, return false
     if (type in codeTypes) return false;
   });
 
