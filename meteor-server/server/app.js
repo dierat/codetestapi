a41f884e10c9267054780d6e35226935b180784a
dierat <diedra.rater@gmail.com>
2016-03-07 21:54:18 -0800

    adds rough plan for checking structure of a program

--- meteor-server/server/app.js
+++ meteor-server/server/app.js
@@ -6,7 +6,16 @@ import esprima from 'esprima';
 'functionality' will be an object literal in the following format:
   {
     shouldHave: [],
+
     shouldNotHave: [],
+
+    structure: [{
+      ForStatement: [{
+        IfStatement: [{
+          WhileStatement: null
+        }]
+      }]
+    }]
   }
 
 'analyzeCode' will return true if the code passed in matches all the requirements
@@ -37,8 +46,31 @@ const analyzeCode = function(code, functionality){
     }
   }
 
+  if (functionality.structure){
+    // for each structural tree
+    for (let k = 0; k < functionality.shouldNotHave.length; k++){
+      const structure = functionality.structure[k];
+
+      // declare a variable to track if the full tree structure has been found
+      // look in parsed.body for an object with a type corresponding with the top of the structure
+      // if you find one that matches, check that object's body and loop through looking for the next type
+      // continue until you reach a value in the structure that is equal to null or until you can't find a type in the structure
+      // if you reach a null value, set the found variable to true and break out of the for loop
+      // if you get to the end of the trees in parsed.body without finding the full structure, return false from the top function
+    }
+  }
+
   // if the code input passed the checks, return true
   return true;
 };
 
 export {analyzeCode};
+
+
+const forLoopWithIf = `
+  for (let i = 0; i < 5; i++){
+    if (i % 2 === 0) console.log(i);
+  }
+`;
+
+console.log(esprima.parse(forLoopWithIf).body[0].body);
