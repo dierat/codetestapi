e771a021a8e60a194a9b6f79b757a81a7205f4b1
dierat <diedra.rater@gmail.com>
2016-03-09 18:49:48 -0800

    refactors shouldHave and shouldNotHave checks to use a helper, adds comments

--- meteor-server/server/app.js
+++ meteor-server/server/app.js
@@ -1,7 +1,8 @@
 import esprima from 'esprima';
 
-/*
 
+
+/*
 'code' will be a string of raw JavaScript
 'functionality' will be an object literal in the following format:
   {
@@ -26,15 +27,37 @@ import esprima from 'esprima';
 'analyzeCode' will return true if the code passed in matches all the requirements
 and false if it does not
 
+Please see the README for more information.
 */
 
 
+
+// 'desiredTypes' will be an array of strings
+// 'haveTypes' will be an object literal with keys for types of code found in input code
+// 'desiredNot' is an optional boolean indicating that the 'desiredTypes' are actually
+// undesired types and the code fails if these are present in the 'haveTypes'
+const checkTypes = function(desiredTypes, haveTypes, desiredNot){
+  for (let i = 0; i < desiredTypes.length; i++){
+    const type = desiredTypes[i];
+    const found = type in haveTypes;
+    if ((!found && !desiredNot) || (found && desiredNot)) return false;
+  }
+
+  return true;
+};
+
+
+
+// helper function for searching the structure of a code tree
+// 'parsedNode' is the current node of the parsed input code
+// 'compareNode' is the current node of the desired code structure
 const searchStructure = function(parsedNode, compareNode){
   // loop through the current student code node
   for (let l = 0; l < parsedNode.length; l++){
-    // if the two current nodes are of the same type
 
+    // if the two current nodes are of the same type
     if (parsedNode[l].type === compareNode.type){
+
       // if this is the last node in the comparison structure, the code passes
       if (!compareNode.child){
         return true;
@@ -49,6 +72,7 @@ const searchStructure = function(parsedNode, compareNode){
             // this function expects parsedNode to be an array
             body = [body];
           }
+
           const foundAll = searchStructure(body, compareNode.child);
           if (foundAll) return true;
         }
@@ -72,27 +96,25 @@ const analyzeCode = function(code, functionality){
 
   // check if each shouldHave is in input code; if not, return false
   if (functionality.shouldHave){
-    for (let i = 0; i < functionality.shouldHave.length; i++){
-      const type = functionality.shouldHave[i];
-      if ( !(type in codeTypes) ) return false;
-    }
+    if ( !checkTypes(functionality.shouldHave, codeTypes, false) ) return false;
   }
 
   // check if each shouldNotHave isn't in input code; if not, return false
   if (functionality.shouldNotHave){
-    for (let j = 0; j < functionality.shouldNotHave.length; j++){
-      const type = functionality.shouldNotHave[j];
-      if (type in codeTypes) return false;
-    }
+    if ( !checkTypes(functionality.shouldNotHave, codeTypes, true) ) return false;
   }
 
+  // check if desired structure exists in input code; if not, return false
   if (functionality.structure){
     const matches = searchStructure(parsed.body, functionality.structure);
     if (!matches) return false;
   }
 
-  // if the code input passed the checks, return true
+  // if the code input passed all the checks, return true
   return true;
 };
 
+
+
+// export this module for other areas of the app to use
 export {analyzeCode};
